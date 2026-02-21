fcac6db954e4a79390412b283e6a05d4c29cf065
RaShunda Lanier <developer@rashundawilliams.com>
2026-02-21 18:40:35 -0500

    add apikey middleware

---
+++ middleware/api_key.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rlanier-webdev/RivalryAPIv2/models"
+	"gorm.io/gorm"
+)
+
+func APIKeyAuth(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("X-API-Key")
+	
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error":"Authorization header required"})
+			return
+		}
+
+		// Validate format
+		if !strings.HasPrefix(authHeader, "riv_") || len(authHeader) != 68 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error":"Authorization header required"})
+			return
+		}
+
+		sum := sha256.Sum256([]byte(authHeader))
+		keyHash := hex.EncodeToString(sum[:])
+
+		var key models.APIKey
+		err := db.Where("key_hash = ? AND is_active = ?", keyHash, true).First(&key).Error
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid or inactive key"})
+			return
+		}
+
+		go db.Model(&key).Update("last_used_at", time.Now())
+
+		c.Set("userID", key.UserID)
+		c.Next()
+
+	}
+	
+}
