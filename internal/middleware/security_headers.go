1cfcce78088fea93a9ba6dc399ad7de78e76d5e9
RaShunda Lanier <developer@rashundawilliams.com>
2026-02-22 16:28:33 -0500

    fixed CSP

--- internal/middleware/security_headers.go
+++ internal/middleware/security_headers.go
@@ -7,7 +7,7 @@ func SecurityHeaders() gin.HandlerFunc {
 		c.Header("X-Content-Type-Options", "nosniff")
         c.Header("X-Frame-Options", "DENY")
         c.Header("Referrer-Policy", "strict-origin-when-cross-origin")
-        c.Header("Content-Security-Policy", "default-src 'none'")
+        c.Header("Content-Security-Policy", "default-src 'none'; style-src 'self' cdn.jsdelivr.net fonts.googleapis.com 'unsafe-inline'; script-src cdn.jsdelivr.net kit.fontawesome.com www.googletagmanager.com 'unsafe-inline'; font-src kit.fontawesome.com fonts.gstatic.com ka-f.fontawesome.com; connect-src www.google-analytics.com ka-f.fontawesome.com cdn.jsdelivr.net; img-src 'self' data:")
         c.Header("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
         c.Next()
 	}
